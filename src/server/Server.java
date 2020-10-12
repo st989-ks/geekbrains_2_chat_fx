253082180348444a9cd27a9796ef7436681b89b8
Stanislav <st989@ya.ru>
2020-10-12 23:53:32 +0400

    Чат

--- src/server/Server.java
+++ src/server/Server.java
@@ -18,12 +18,12 @@ public class Server {
         final int PORT = 8189;
 
         try {
-            server = new ServerSocket(PORT);
-            System.out.println("Server started");
+            server = new ServerSocket( PORT );
+            System.out.println( "Server started" );
 
             while (true) {
                 socket = server.accept();
-                new ClientHandler(this, socket);
+                new ClientHandler( this, socket );
             }
 
         } catch (IOException e) {
@@ -43,21 +43,43 @@ public class Server {
     }
 
     public void broadcastMsg(ClientHandler sender, String msg) {
-        String message = String.format("[ %s ]: %s", sender.getNickname(), msg);
+        String message = String.format( "Сообщение всем от [ %s ]:\n%s", sender.getNickname(), msg );
         for (ClientHandler c : clients) {
-            c.sendMsg(message);
+            c.sendMsg( message );
         }
     }
 
     public void subscribe(ClientHandler clientHandler) {
-        clients.add(clientHandler);
+        clients.add( clientHandler );
     }
 
     public void unsubscribe(ClientHandler clientHandler) {
-        clients.remove(clientHandler);
+        clients.remove( clientHandler );
     }
 
     public AuthService getAuthService() {
         return authService;
+    }
+
+    public void sendPrivate(ClientHandler client, String nickname, String msg) {
+        for (ClientHandler c : clients) {
+
+            if (c.getNickname().equals( nickname )) {
+                c.sendMsg( "Сообщение от [ " + client.getNickname() + " ]:\n" + msg );
+                client.sendMsg( "Сообщение пользователю [ " + c.getNickname() + " ]:\n" + msg );
+                return;
+            }
+
+        }
+        client.sendMsg( "Ошибка:\nПользователь [ " + nickname + " ] отсутствует в чате" );
+    }
+
+    public boolean repeatNickname(String nick) {
+        for (ClientHandler c : clients) {
+            if (c.getNickname().equals( nick )) {
+                return true;
+            }
+        }
+        return false;
     }
 }
